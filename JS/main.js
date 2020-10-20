239c5edb742a242459b782c825ea3584d28433cd
RaquelGarciaCaja <raquelgc8@gmail.com>
2020-10-20 14:40:13 +0200

    coplete mid evaluation

--- JS/main.js
+++ JS/main.js
@@ -10,46 +10,39 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 const numberRandom = getRandomNumber(100);
-console.log(numberRandom);
 
 // Hacer el sumatorio
 let acc = 0;
-console.log(acc);
 
 function sumAtt() {
   // inicia el incremento
   attempt.innerHTML = acc++;
-  console.log(attempt.innerHTML);
+}
+
+function handlerClick() {
+  PaintListener();
+  sumAtt();
 }
 
 // función listener
-function go() {
+function PaintListener() {
   // cambiar pista
+  // coger el valor
   const inputValue = parseInt(input.value);
-  console.log(inputValue);
   // condicionales de clue.Entrando en el if
   if (inputValue > 100 || inputValue < 0) {
     clue.innerHTML = "El número debe estar entre 1 y 100";
-    console.log("El número debe estar entre 1 y 100");
     // condicionales de clue.Saliendo del if
   } else {
     console.log(inputValue, numberRandom);
     if (inputValue > numberRandom) {
       clue.innerHTML = "Demasiado alto";
-      console.log("Demasiado alto");
     } else if (inputValue < numberRandom) {
       clue.innerHTML = "Demasiado bajo";
-      console.log("Demasiado bajo");
     } else if (inputValue === numberRandom) {
       clue.innerHTML = "Has ganado campeona!!!";
-      console.log("Has ganado campeona!!!");
     }
   }
-
-  // sumar intentos
-  sumAtt();
 }
 
-go();
-
-btn.addEventListener("click", go);
+btn.addEventListener("click", handlerClick);
